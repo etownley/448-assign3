3bb5160200dd31d14d7fac5d33f868b05b893c6b
Emma Townley-Smith <emmats@stanford.edu>
2016-04-23 23:47:21 -0700

    Started adding interactive filters

--- interaction.js
+++ interaction.js
@@ -10,8 +10,16 @@ var projection = d3.geo.mercator()
 	.scale(225000)
 	.translate([width / 2, height / 2]);
 
+var incidents = [];
+
+var options = {
+	"DayOfWeek": [],
+	"TimeRange": ["Morning", "Afternoon", "Night"],
+	"Category": ["OTHER OFFENSES"]
+};
+
 // Add an svg element to the DOM
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#map").append("svg")
 	.attr("width", width)
 	.attr("height", height);
 
@@ -23,53 +31,83 @@ svg.append("image")
 
 // -------------- LINK ARRAY OF INCIDENTS TO DOM ELEMENTS ---------------
 function drawPoints() {
-	d3.select("svg")
+	var filteredIncidents = incidents.filter(function(incident) {
+		return incident.Selected;
+	});
+
+	var map = d3.select("svg")
 		.selectAll("circle")
-		.data(allIncidents)
-		.enter()
+		.data(filteredIncidents);
+
+	map.enter()
 		.append("circle")
+		.filter(function(d) { return d.Selected })
 		.attr("cx", function(d) { return projection(d.Location)[0] })
 		.attr("cy", function(d) { return projection(d.Location)[1] })
 		.attr("r", "1px")
-		.attr("fill", "red");
+		.attr("fill", "rgba(255, 0, 0, 0.3)");
+
+	map.exit().remove();
 }
 
 // --------------------- EXTRACT DATA FROM JSON FILE --------------------
 
-var allIncidents = [];
+function convertTimeToRange(time) {
+  var partsOfTime = time.split(":");
+	var hour = parseInt(partsOfTime[0]);
+	if (hour >= 6 && hour < 12) {
+		return "Morning";
+	} else if (hour >= 12 && hour < 19) {
+		return "Afternoon";
+	} else {
+		return "Night";
+	}
+}
+
+/**
+ * This function selects/deselects incidents based on criteria as specified in
+ * options object.
+ *
+ * Potential options
+ * {
+ *   "DayOfWeek": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+ *   "TimeRange": ["Morning", "Afternoon", "Night"],
+ *   "Category": [...]
+ * }
+ */
+function filterIncidents(options) {
+	incidents.forEach(function(incident, index, arr) {
+		if (options.DayOfWeek.indexOf(incident.DayOfWeek) < 0) {
+			arr[index].Selected = false;
+			return;
+		}
+
+		if (options.Category.indexOf(incident.Category) < 0) {
+			arr[index].Selected = false;
+			return;
+		}
+
+		if (options.TimeRange.indexOf(incident.TimeRange) < 0) {
+			arr[index].Selected = false;
+			return;
+		}
+
+		arr[index].Selected = true;
+	});
+}
 
 d3.json("scpd_incidents.json", function(error, scpd_incidents) {
 	if (error) return console.warn(error);
-	console.log(scpd_incidents);
 	scpd_incidents.data.forEach(function(d) { // for each row in data table
 		//d.Date = format.parse(d.date); -- need to figure this out later
-		allIncidents.push(d); // put dynamically created object into array
+		d.Selected = true; // set default selected property to true
+		d.TimeRange = convertTimeToRange(d.Time);
+		incidents.push(d); // put dynamically created object into array
 	});
-	console.log(allIncidents);
-	drawPoints();
 
+	drawPoints();
 	// note to self location is an array with lat and long
 });
-
-
-
-
-
-
-/*
-
-What to do next
-
-1. Figure out how to read JSON file into the browser
-	- basically involves making extract_data.js work correctly
-	- convert into an array of Javascript objects
-2a.Figure out how to add array of Javascript location objects produced
-   in first step to the map.
-    - involves some projection voodoo
-2b.Figure out how to filter array of Javascript objects based on proximity
-    - given (lat, long) find everything within radius
-    - optionally add in filtering controls
-*/
 
 function findNearestCrimes(latitude, longitude, radius, options) {
 	for (incident in incidents) {
@@ -79,14 +117,33 @@ function findNearestCrimes(latitude, longitude, radius, options) {
 				// if not in filter then throw it
 			}
 			// filter by all crime types specified
-		}		
+		}
 	}
 }
 
-// Example call of this function
-findNearestCrimes(35, 35, 5, {
-	"crime_types": [ "theft", "homicide" ]
-})
+function attachDayListeners() {
+	var buttons = document.getElementsByClassName("day-btn");
 
+	var handleClick = function(e) {
+		var DayOfWeek = e.target.value;
+		var existingIndex = options.DayOfWeek.indexOf(DayOfWeek);
+		if (existingIndex > -1) {
+			options.DayOfWeek.splice(existingIndex, 1);
+			e.target.classList.remove("active");
+		} else {
+			options.DayOfWeek.push(DayOfWeek);
+			e.target.classList.add("active");
+		}
 
+		filterIncidents(options);
+		drawPoints();
+	};
 
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", handleClick);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	attachDayListeners();
+});
