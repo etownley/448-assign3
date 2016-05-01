682a673f0e84bb8da707d52e9809cb91e75f3776
Emma Townley-Smith <emmats@stanford.edu>
2016-05-01 15:27:27 -0700

    Default button state is now correct. Now updating the look/interaction with ALL, RESET, etc...

--- interaction.js
+++ interaction.js
@@ -25,7 +25,7 @@ var selectRadius = 0.05; //Currently I hard-coded the radius just to test out fu
 
 
 var options = {
-	"DayOfWeek": ["Sunday", "Monday", "Tuesday","Wedndesday", "Thursday", "Friday", "Saturday"],
+	"DayOfWeek": ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"],
 	"TimeRange": ["Morning", "Afternoon", "Night", "Late Night"],
 	"Category": ["ARSON", "ASSAULT", "BRIBERY", "BURGLARY", "DISORDERLY CONDUCT", "DRIVING UNDER THE INFLUENCE", "DRUG/NARCOTIC", "DRUNKENNESS", "EMBEZZLEMENT", "EXTORTION", "FAMILY OFFENSES", "FORGERY/COUNTERFEITING", "FRAUD", "GAMBLING", "KIDNAPPING", "LARCENY/THEFT", "LIQUOR LAWS", "LOITERING", "MISSING PERSON", "NON-CRIMINAL", "OTHER OFFENSES", "PROSTITUTION", "ROBBERY", "RUNAWAY", "SECONDARY CODES", "SEX OFFENSES, FORCIBLE", "SEX OFFENSES, NON FORCIBLE", "STOLEN PROPERTY", "SUICIDE", "SUSPICIOUS OCC", "TRESPASS", "VANDALISM", "VEHICLE THEFT", "WARRANTS", "WEAPON LAWS"]
 };
@@ -131,6 +131,11 @@ d3.json("scpd_incidents.json", function(error, scpd_incidents) {
 	});
 
 	drawPoints();
+
+	var buttons = document.getElementsByClassName("default-selected");
+	for(var i = 0; i < buttons.length; i++) {
+		buttons[i].classList.add("active");
+	}
 	//emptyOptions();
 	// note to self location is an array with lat and long
 });
@@ -308,7 +313,6 @@ function attachResetListener() {
 	var buttons = document.getElementsByClassName("reset-btn");
 
 	var handleClick = function(e) {
-		//BUG HERE: add all back into the category
 		options = {
 			"DayOfWeek": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
 			"TimeRange": ["Morning", "Afternoon", "Night", "Late Night"],
